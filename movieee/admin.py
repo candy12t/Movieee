658e79e1ca55f3dd477b7adcbbfda5cc972e4fd6
candy12t <candy12t@gmail.com>
2019-09-09 22:47:21 +0900

    adminサイトで表示するカラムの変更

--- movieee/admin.py
+++ movieee/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-  list_display = ('id', 'title', 'text',)
-  list_display_links = ('title',)
+  list_display = ('id', 'title','created_date', 'updated_date',)
+  list_display_links = ('id', 'title',)
 
 admin.site.register(Post, PostAdmin)
