3a940d86ced7fa003878f518df9d550c9b891018
candy12t <candy12t@gmail.com>
2019-09-09 23:03:29 +0900

    投稿内容を新しい順にする

--- movieee/views.py
+++ movieee/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from .models import Post
 
 def index(request):
-  return render(request, 'movieee/index.html')
+  posts = Post.objects.all().order_by('-created_date')
+  return render(request, 'movieee/index.html', {'posts': posts})
 
 
 def users(request, pk):
