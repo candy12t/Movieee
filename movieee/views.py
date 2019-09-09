b079ada0511a9bc636bcd2699428ebd83d5f61cd
candy12t <candy12t@gmail.com>
2019-09-09 18:16:13 +0900

    users関数の作成

--- movieee/views.py
+++ movieee/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 def index(request):
   return render(request, 'movieee/index.html')
+
+
+def users(request, pk):
+  user = get_object_or_404(User, pk=pk)
+  return render(request, 'movieee/users.html', {'user': user})
