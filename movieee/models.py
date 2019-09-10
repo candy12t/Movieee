d51eb19f93b33645930fcfbfe5ab4d908686dd3d
candy12t <candy12t@gmail.com>
2019-09-10 23:13:47 +0900

    PostモデルとUserモデルを紐付ける

--- movieee/models.py
+++ movieee/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
   title = models.CharField(max_length=100)
   text = models.TextField()
   image = models.ImageField(upload_to='image')
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   created_date = models.DateTimeField(auto_now_add=True)
   updated_date = models.DateTimeField(auto_now=True)
 
