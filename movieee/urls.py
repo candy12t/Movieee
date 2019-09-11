9c5814fbee6e0e4c15fb9606ae4677fa77c15615
candy12t <candy12t@gmail.com>
2019-09-11 19:52:25 +0900

    signupのurlの修正

--- movieee/urls.py
+++ movieee/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
   path('users/<int:pk>/', views.users_detail, name='users_detail'),
   path('login/', auth_views.LoginView.as_view(template_name='movieee/login.html'), name='login'),
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-  path('singup/', views.singup, name='signup'),
+  path('signup/', views.signup, name='signup'),
 ]
