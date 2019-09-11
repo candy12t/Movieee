ef8aaaf707fa0f500d2b152bba3b2d95468ad1be
candy12t <candy12t@gmail.com>
2019-09-11 23:59:29 +0900

    認証し、ログインする

--- movieee/views.py
+++ movieee/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post
@@ -15,9 +16,16 @@ def users_detail(request, pk):
 
 def signup(request):
   if request == 'POST':
-    form =  UserCreationForm(request.POST)
+    form = UserCreationForm(request.POST)
     if form.is_valid():
       new_user = form.save()
+      input_username = form.cleaned_data['username']
+      input_password = form.cleaned_data['password']
+      # フォームの入力値が認証できればユーザーオブジェクト、できなければNoneを返す
+      new_user = authenticate(username=input_username, password=input_password)
+      if new_user is not None:
+        login(request, new_user)
+        return redirect('movieee:users_detail', pk=new_user.pk)
   else:
     form = UserCreationForm()
   return render(request, 'movieee/signup.html', {'form': form})
