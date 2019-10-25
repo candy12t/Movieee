493c3ab2378f451a36b3baeed8cbb7e71d284d49
candy12t <candy12t@gmail.com>
2019-10-25 14:56:41 +0900

    インストールするアプリの書き方変更

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movieee',
+    'movieee.apps.MovieeeConfig',
     'django_cleanup',
 ]
 
