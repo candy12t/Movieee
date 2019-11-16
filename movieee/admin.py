fe1b4ac1a9d94bb8fc9be7f44348a6be3c223a55
candy12t <candy12t@gmail.com>
2019-11-16 18:00:46 +0900

    管理サイトにコメントモデルの表示

--- movieee/admin.py
+++ movieee/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
   list_display = ('id', 'title','created_date', 'updated_date',)
   list_display_links = ('id', 'title',)
 
+
+class CommentAdmin(admin.ModelAdmin):
+  list_display = ('id', 'comment','created_date',)
+  list_display_links = ('id', 'comment',)
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
