eca456e1e88649ddc2db03793f6a3ab5252527f6
candy12t <candy12t@gmail.com>
2019-12-29 23:44:10 +0900

    新規登録のurl削除

--- movieee/urls.py
+++ movieee/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
   path('users/<int:pk>/', views.users_detail, name='users_detail'),
   path('login/', auth_views.LoginView.as_view(template_name='movieee/login.html'), name='login'),
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-  path('signup/', views.signup, name='signup'),
   path('posts/new/', views.posts_new, name='posts_new'),
   path('posts/<int:pk>/', views.posts_detail, name='posts_detail'),
   path('posts/<int:pk>/edit/', views.posts_edit, name='posts_edit'),
