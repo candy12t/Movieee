83c41e51fa2b9d9eefc0ffc97296a471a8bde06e
candy12t <candy12t@gmail.com>
2019-12-30 14:33:43 +0900

    ログアウトurl追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
   path('signup/', views.signup, name='signup'),
   path('login/', auth_views.LoginView.as_view(template_name='movieee/login.html'), name='login'),
+  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
