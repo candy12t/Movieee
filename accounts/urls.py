7cf8328842ce9a9f20ef39400336183dbf610354
candy12t <candy12t@gmail.com>
2019-12-30 14:45:26 +0900

    ユーザー詳細のurl追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
   path('signup/', views.signup, name='signup'),
   path('login/', auth_views.LoginView.as_view(template_name='movieee/login.html'), name='login'),
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+  path('users/<int:pk>/', views.users_detail, name='users_detail'),
 ]
