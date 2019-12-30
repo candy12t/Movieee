02f8e2feaffb2da1b2caed2f2ed377b1679d2570
candy12t <candy12t@gmail.com>
2019-12-30 14:33:26 +0900

    ログアウトurl削除

--- movieee/urls.py
+++ movieee/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('users/<int:pk>/', views.users_detail, name='users_detail'),
   # path('login/', auth_views.LoginView.as_view(template_name='movieee/login.html'), name='login'),
-  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+  # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
   path('posts/new/', views.posts_new, name='posts_new'),
   path('posts/<int:pk>/', views.posts_detail, name='posts_detail'),
   path('posts/<int:pk>/edit/', views.posts_edit, name='posts_edit'),
