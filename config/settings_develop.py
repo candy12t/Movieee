a287c60dd0e744e1ebfb2cfb5a84fdc19afd1522
candy12t <candy12t@gmail.com>
2020-02-26 00:58:33 +0900

    開発用のロガーの設定

--- config/settings_develop.py
+++ config/settings_develop.py
@@ -6,3 +6,43 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    # ログフォーマット
+    'formatters': {
+        # 開発用
+        'develop': {
+            'format': '%(asctime)s [%(levelname)s] %(pathname)s:%(lineno)d'
+                      '%(message)s'
+        },
+    },
+
+    # ハンドラ
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'develop',
+        },
+    },
+
+    # ロガー
+    'loggers': {
+        # movieeeアプリケーションのロガー
+        'movieee': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+
+        # Django本体のロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
