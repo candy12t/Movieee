bb896dae9e39d451dc213aaaed5b4bba59eef8ba
candy12t <candy12t@gmail.com>
2020-03-07 22:42:23 +0900

    #8 AbstractBaseUserを継承したカスタムユーザーモデルの作成

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,14 @@
+import uuid
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import UserManager, PermissionsMixin
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
-#  カスタムユーザーマネジャー
 class CustomUserManager(UserManager):
-
     use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
@@ -32,3 +35,65 @@ class CustomUserManager(UserManager):
             raise ValueError('Superuser must have is_superuser=True.')
 
         return self._create_user(email, password, **extra_fields)
+
+
+# カスタムユーザー メールアドレスで認証
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    username_validator = UnicodeUsernameValidator()
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_name = models.CharField(
+        _('username'),
+        max_length=30,
+        help_text=_('Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+    )
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        help_text=_('Required.'),
+    )
+    icon = models.ImageField(_('アイコン'), upload_to='icon', blank=True)
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = CustomUserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        return self.user_name
+
+    def get_short_name(self):
+        return self.user_name
+
+    def email_user(self, subject, message, form_email=None, **kwargs):
+        send_mail(subject, message, form_email, [self.email], **kwargs)
+
+    @property
+    def username(self):
+        return self.email
