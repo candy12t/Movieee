a74e84c42a7f0dd1cc543d3e31461c19ed540def
candy12t <candy12t@gmail.com>
2020-03-15 20:53:17 +0900

    #8 ログアウトurl追記

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('', views.index, name='index'),
 ]
