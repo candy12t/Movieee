904e2db7a3e6bf4a04f5ffd913283feb46246714
candy12t <candy12t@gmail.com>
2020-03-16 12:57:20 +0900

    #8 修正

--- movieee/views.py
+++ movieee/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.models import User
 from django.contrib import messages
 from .form import PostForm, CommentForm
 from .models import Post, Comment
