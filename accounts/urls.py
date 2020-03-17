370bc57acc9b5596064383632ac4323b3ca932c7
candy12t <candy12t@gmail.com>
2020-03-17 13:53:28 +0900

    #8 パスワード変更url及びパスワード変更完了urlの作成

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
     path('users/<str:pk>/', views.users_detail, name='users_detail'),
+    path('password_change/', views.password_change, name='password_change'),
+    path('password_change/done/', views.password_change_done, name='password_change_done'),
 ]
