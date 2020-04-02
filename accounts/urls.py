573c47c685edacedb39ae59e807093fd119beacb
candy12t <candy12t@gmail.com>
2020-04-02 17:21:36 +0900

    #12 プロフィール変更url作成

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
     path('users/<str:pk>/', views.users_detail, name='users_detail'),
+    path('users/<str:pk>/profile_change/', views.profile_change, name='profile_change'),
     path('password_change/', views.password_change, name='password_change'),
     path('password_change/done/', views.password_change_done, name='password_change_done'),
 ]
